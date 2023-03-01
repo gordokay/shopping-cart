c31f934ce36c5aa7221a11967105d1f8f6921d2f
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-03-01 12:16:12 -0500

    Create list of store items

---
+++ src/items.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    id: '#AFA2FF',
+    name: 'Tropical Indigo',
+    price: 12.50
+  },
+  {
+    id: '#E3C0D3',
+    name: 'Thistle',
+    price: 14.25
+  },
+  {
+    id: '#0B132B',
+    name: 'Oxford Blue',
+    price: 5.00
+  },
+  {
+    id: '#EDEBA0',
+    name: 'Vanilla',
+    price: 7.50
+  },
+  {
+    id: '#D3FAD6',
+    name: 'Tea Green',
+    price: 17.60
+  }
+]
+
+export default items;
