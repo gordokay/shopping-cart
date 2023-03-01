17c17ce9b6a3b04bc8d51c40c4094bf684ed9606
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-03-01 12:58:22 -0500

    Add state and set shop items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
 
+import items from './items';
+
 function App() {
+  const [itemInfo, setItemInfo] = useState({});
+  const [cartInfo, setCartInfo] = useState({
+    cartItems: [],
+    total: 0
+  });
+
+  useEffect(() => {
+    const shopItems = {};
+    items.forEach(item => {
+      shopItems[item.id] = {
+        quantity: 0,
+        price: item.price
+      }
+    })
+    setItemInfo(shopItems);
+  }, [])
+  
   return (
     <BrowserRouter>
       <Navigation />
